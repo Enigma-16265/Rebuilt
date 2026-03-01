799fc6d97c4c3ee507436720c8151595de385b89
Code Mac <code.mac@eenigmarobotics.org>
2026-03-01 14:14:58 -0700

    ci: Update the canid/ports and set everything to Canivore

--- KirbyBotToo/src/main/java/frc/robot/Ports.java
+++ KirbyBotToo/src/main/java/frc/robot/Ports.java
@@ -8,16 +8,16 @@ public final class Ports {
     public static final CANBus kCANivoreCANBus = new CANBus("main");
 
     // Talon FX IDs
-    public static final int kIntakePivot = 10;
-    public static final int kIntakeRollers = 11;
-    public static final int kFloor = 12;
-    public static final int kFeeder = 13;
-    public static final int kShooterLeft = 14;
-    public static final int kShooterMiddle = 15;
-    public static final int kShooterRight = 16;
-    public static final int kHanger = 18;
+    public static final int kIntakePivot = 52;
+    public static final int kIntakeRollers = 51;
+    public static final int kFloor = 54;
+    public static final int kFeeder = 55;
+    public static final int kShooterLeft = 56;
+    public static final int kShooterMiddle = 57;
+    public static final int kShooterRight = 58;
+    public static final int kHanger = 53;
 
     // PWM Ports
-    public static final int kHoodLeftServo = 3;
-    public static final int kHoodRightServo = 4;
+    public static final int kHoodLeftServo  = 8;
+    public static final int kHoodRightServo = 9;
 }
