a86a877c1982b6ccaa80ec2e697e4e324cc59ef2
Driver Mac <driver.mac@enigmarobotics.org>
2026-04-03 13:18:56 -0600

    Fine tuned the shooter and feeder constants

--- KirbyBotToo/src/main/java/frc/robot/subsystems/Floor.java
+++ KirbyBotToo/src/main/java/frc/robot/subsystems/Floor.java
@@ -22,7 +22,7 @@ import frc.robot.Ports;
 public class Floor extends SubsystemBase {
     public enum Speed {
         STOP(0),
-        FEED(0.83);
+        FEED(0.87);
 
         private final double percentOutput;
 
--- KirbyBotToo/src/main/java/frc/robot/subsystems/Shooter.java
+++ KirbyBotToo/src/main/java/frc/robot/subsystems/Shooter.java
@@ -35,7 +35,7 @@ public class Shooter extends SubsystemBase {
     private final VelocityVoltage velocityRequest = new VelocityVoltage(0).withSlot(0);
     private final VoltageOut voltageRequest = new VoltageOut(0);
 
-    private double dashboardTargetRPM = 5000.0;
+    private double dashboardTargetRPM = 3500.0;
 
     public Shooter() {
         leftMotor = new TalonFX(Ports.kShooterLeft, Ports.kCANivoreCANBus);
